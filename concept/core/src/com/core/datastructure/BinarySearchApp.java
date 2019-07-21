544867bee8d5bd0939edf5a861292749fe6b0eed
sunsingh <sunsingh@sunsingh-WX-4.corp.adobe.com>
2019-07-21 14:43:07 +0530

    changes in notes

--- concept/core/src/com/core/datastructure/BinarySearchApp.java
+++ concept/core/src/com/core/datastructure/BinarySearchApp.java
@@ -8,33 +8,38 @@ public class BinarySearchApp {
 
     public static void main(String[] args) throws Exception {
         int array[] = {1, 3, 6, 99, 102, 120};
-        int index = binarySearch(array, 0, array.length - 1, 120);
+        int index = recursiveBinarySearch(array, 0, array.length - 1, 120);
         System.out.println(index);
     }
 
-    public static int binarySearch(int arr[], int fromIndex, int toIndex, int item) {
-        if (toIndex >= fromIndex) {
+    //Method 1
+    public static int recursiveBinarySearch(int arr[], int fromIndex, int toIndex, int item) {
+        if (fromIndex <= toIndex) {
             int midIndex = fromIndex + (toIndex - fromIndex) / 2;
-
-            // If the element is present at the 
-            // middle itself 
             if (arr[midIndex] == item) {
                 return midIndex;
             }
-
-            // If element is smaller than mid, then 
-            // it can only be present in left subarray 
             if (arr[midIndex] > item) {
-                return binarySearch(arr, fromIndex, midIndex - 1, item);
+                return recursiveBinarySearch(arr, fromIndex, midIndex - 1, item);
+            }
+            return recursiveBinarySearch(arr, midIndex + 1, toIndex, item);
+        }
+        return -1;
+    }
+
+    //Method 2
+    public static int iteratrorBinarySearch(int[] array, int key, int fromIndex, int toIndex) {
+        while (fromIndex <= toIndex) {
+            int midIndex = fromIndex + (toIndex - fromIndex) / 2;
+            if (key == array[midIndex]) {
+                return midIndex;
+            } else if (key > array[midIndex]) {
+                fromIndex = midIndex + 1;
+            } else {
+                toIndex = midIndex - 1;
             }
 
-            // Else the element can only be present 
-            // in right subarray 
-            return binarySearch(arr, midIndex + 1, toIndex, item);
         }
-
-        // We reach here when element is not present 
-        // in array 
         return -1;
     }
 
