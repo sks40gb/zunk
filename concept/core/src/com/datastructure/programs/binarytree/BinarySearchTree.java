38ab4933f7e98f6c44a0c31c210483bfad9d5057
sunsingh <sunsingh@sunsingh-WX-4.corp.adobe.com>
2019-08-14 19:45:10 +0530

    Datastructure

--- concept/core/src/com/datastructure/programs/binarytree/BinarySearchTree.java
+++ concept/core/src/com/datastructure/programs/binarytree/BinarySearchTree.java
@@ -7,10 +7,10 @@ import java.util.concurrent.LinkedBlockingQueue;
  *
  * @author sunsingh
  */
-public class BinaryTree<T extends Integer> {
+public class BinarySearchTree<T extends Integer> {
 
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
+        BinarySearchTree tree = new BinarySearchTree();
         tree.insert(5);
         tree.insert(3);
         tree.insert(7);
