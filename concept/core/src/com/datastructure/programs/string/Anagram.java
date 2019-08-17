a1a3f54a36b1ccb58586d3dd79f606dbef48df2b
sunil <sunil@192.168.0.3>
2019-08-17 16:40:57 +0530

    Datastructure

---
+++ concept/core/src/com/datastructure/programs/string/Anagram.java
@@ -0,0 +1,39 @@
+package com.datastructure.programs.string;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author sunil
+ */
+public class Anagram {
+    
+    public static void main(String[] args) {
+        String s1 =  "wait";
+        String s2 = "twai";
+        String s3 = "wiak";
+        
+        System.out.println(isAnagram(s1, s2));
+        System.out.println(isAnagram(s1, s3));
+        
+    }
+    
+    public static boolean isAnagram(String s1,String s2){
+        if(s1 == null || s2 == null && s1.length() != s2.length()){
+            return false;
+        }else{
+            char[] arr1 = s1.toCharArray();
+            char[] arr2 = s2.toCharArray();
+            
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            for(int i=0; i < arr1.length; i++){
+                if(arr1[i] != arr2[i]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+}
---
+++ concept/core/src/com/datastructure/programs/string/FindDuplicate.java
@@ -0,0 +1,37 @@
+package com.datastructure.programs.string;
+
+/**
+ *
+ * @author sunil
+ */
+public class FindDuplicate {
+
+    static final int NO_OF_CHARS = 256;
+    
+    // Driver Method 
+    public static void main(String[] args) {
+        String str = "test string";
+        printDups(str);
+    }
+
+    /* Fills count array with frequency of characters */
+    static void fillCharCounts(String str, int[] count) {
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i)]++;
+        }
+    }
+
+    /* Print duplicates present in the passed string */
+    static void printDups(String str) {
+        // Create an array of size 256 and fill count of every character in it 
+        int count[] = new int[NO_OF_CHARS];
+        fillCharCounts(str, count);
+
+        for (int i = 0; i < NO_OF_CHARS; i++) {
+            if (count[i] > 1) {
+                System.out.printf("%c,  count = %d \n", i, count[i]);
+            }
+        }
+
+    }
+}
