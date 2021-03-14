84aabcae93944fdaa1c06476cc84a92474b8f75c
Sunil <sks.256@gmail.com>
2021-03-14 17:33:08 +0530

    streams

---
+++ concept/core/src/com/core/stream/g_joining/Person.java
@@ -0,0 +1,24 @@
+package com.core.stream.g_joining;
+
+public class Person {
+  private final String name;
+  private final int age;
+  
+  public Person(String theName, int theAge) {
+    name = theName;
+    age = theAge;
+  }
+  
+  public String getName() { 
+    return name; 
+  }
+
+  public int getAge() { 
+    return age; 
+  }
+  
+  public String toString() {
+    return String.format("%s -- %d", name, age);
+  }  
+}
+
---
+++ concept/core/src/com/core/stream/i_and_mapping/Person.java
@@ -0,0 +1,24 @@
+package com.core.stream.i_and_mapping;
+
+public class Person {
+  private final String name;
+  private final int age;
+  
+  public Person(String theName, int theAge) {
+    name = theName;
+    age = theAge;
+  }
+  
+  public String getName() { 
+    return name; 
+  }
+
+  public int getAge() { 
+    return age; 
+  }
+  
+  public String toString() {
+    return String.format("%s -- %d", name, age);
+  }  
+}
+
