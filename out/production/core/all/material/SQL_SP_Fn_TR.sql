13dd01f77675446ed69ac677fee3d545f7c41679
Sunil <sks.256@gmail.com>
2021-03-14 17:33:23 +0530

    streams

---
+++ out/production/core/all/material/SQL_SP_Fn_TR.sql
@@ -0,0 +1,262 @@
+STORED PROCEDURE
+=======================================================================================================================
+VIP
+========================================================================================================================
+There must be space between DELIMITER and delimiter value
+ex : DELIMITER $$
+
+After end of all statement semicolon(;) is required.
+ex :
+END IF;
+
+
+
+http://forums.mysql.com/read.php?98,358569
+------------------------------------------------
+1. Stored procedure reduced the traffic between application and database server because instead of sending multiple
+uncompiled long SQL commands statement, application only has to send the stored procedure name and get the result back.
+2. Reusable and transparent - to any application which wants to use it.
+3. Secured - Database administrator can grant the right to application.
+
+Disadvantages
+-------------
+1. Database server high load in both memory and processors
+2. You could be asking the database server to perform a number of logical operations or a complex of business logic 
+which is not the role of it.
+3. contains declarative SQL so it is very difficult to write a procedure with complexity of business like other languages.
+4. Cannot debug stored procedure in almost RDMBSs.
+5. Writing and maintain stored procedure usually required specialized skill set.
+
+
+The first stored procedure is very simple. It retrieves all products from products table. First let’s take a look at the 
+stored procedure source code bellow:
+############################################################################################
+DELIMITER //
+CREATE PROCEDURE GetAllProducts()
+BEGIN
+SELECT * FROM products;
+END //
+DELIMITER ;
+############################################################################################
+
+* The first command you see is DELIMITER //. This command is not related to the stored procedure. DELIMITER statement is 
+used to change the standard delimiter (semicolon) to another
+
+CALL GetAllProducts();
+
+
+Declaring variables
+-------------------
+DECLARE variable_name datatype(size) DEFAULT default_value;
+DECLARE x, y INT DEFAULT 0;
+
+Assigning variables
+-------------------
+DECLARE total_count INT DEFAULT 0
+SET total_count = 10;
+
+DECLARE total_products INT DEFAULT 0;
+SELECT COUNT(*) INTO total_products
+
+
+Stored Procedure Parameters
+----------------------------
+* a parameter has one of three modes
+1. IN    - indicates that a parameter can be passed into stored procedures but any modification inside stored procedure 
+does not change parameter
+2. OUT   - indicates that stored procedure can change this parameter and pass back to the calling program.
+3. INOUT -  mode is combined of IN and OUT mode; you can pass parameter into stored procedure and get it back with the 
+new value from calling program.
+
+############################################################################################
+DELIMITER $$
+
+CREATE PROCEDURE getTotalFileNumber(IN p_file_type INT(100), OUT total INT)
+BEGIN
+   SELECT COUNT(*) INTO total FROM fcl_file_number WHERE file_type = p_file_type ;
+END$$
+
+DELIMITER ;
+
+CALL getTotalFileNumber('EXPORT',@total);
+SELECT @total AS Total;
+
+############################################################################################
+
+@<variable_name> - it will store the variable value in session
+ex : @total
+
+
+How to retreive multiple rows from stored procedure in mysql?
+------------------------------------------------------------------------------------------------
+Plan A: Fill another table, it may be a temporary table.
+Plan B: Just execute your SELECT statement without INTO clause from the procedure; then you could read data-set from the application (c#, PHP+mysqli,...)
+Plan C: Do not use the procedure, just execute the SELECT query.
+
+
+Conditional Control
+------------------------------------------------------------------------------------------------
+IF expression THEN commands
+[ELSEIF expression THEN commands]
+[ELSE commands]
+END IF;
+
+** Don't forget to use THEN
+
+
+
+CASE
+WHEN expression THEN commands
+…
+WHEN expression THEN commands
+ELSE commands
+END CASE;
+
+
+Loop
+--------------------------------
+
+1. WHILE
+
+WHILE expression DO
+Statements
+END WHILE;
+
+
+2. REPEAT
+
+REPEAT
+Statements;
+UNTIL expression
+END REPEAT;
+
+
+3. LOOP
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS LOOPLoopProc$$
+CREATE PROCEDURE LOOPLoopProc()
+BEGIN
+DECLARE x INT;
+DECLARE str VARCHAR(255);
+SET x = 1;
+SET str = '';
+loop_label: LOOP
+IF x > 10 THEN
+LEAVE loop_label;
+END IF;
+SET x = x + 1;
+IF (x mod 2) THEN
+ITERATE loop_label;
+ELSE
+SET str = CONCAT(str,x,',');
+END IF;
+
+END LOOP;
+SELECT str;
+END$$
+DELIMITER;
+
+
+
+4. SQL Cursor - Cursor is used to iterate through a set of rows, which returned by a query, and process individual row
+
+** One of the most important point when working with cursor is you should use a NOT FOUND handler to avoid raising a 
+fatal “no data to fetch” condition.
+
+DECLARE cursor_name CURSOR FOR SELECT_statement;
+
+MySQL supports following statements for working with cursor.
+First you have to declare a cursor using DECLARE statement:
+DECLARE cursor_name CURSOR FOR SELECT_statement;
+Second you have to open the cursor using OPEN statement. You must open cursor before fetching rows from it.
+OPEN cursor_name;
+Next you can retrieve next row from cursor and move the cursor to the following row in a result set by using FETCH statement.
+FETCH cursor_name INTO variable list;
+And finally, you must close the cursor to deactivate it and release the memory associated with that cursor. To close the
+ cursor you use CLOSE statement:
+CLOSE cursor_name;
+
+
+############################################################################################
+DELIMITER $$
+DROP PROCEDURE IF EXISTS CursorProc$$
+CREATE PROCEDURE CursorProc()
+BEGIN
+DECLARE no_more_products, quantity_in_stock INT DEFAULT 0;
+DECLARE prd_code VARCHAR(255);
+DECLARE cur_product CURSOR FOR SELECT productCode FROM products;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET no_more_products = 1;
+
+/* for loggging information */
+CREATE TABLE infologs (
+Id int(11) NOT NULL AUTO_INCREMENT,
+Msg varchar(255) NOT NULL,
+PRIMARY KEY (Id)
+);
+OPEN cur_product;
+
+FETCH cur_product INTO prd_code;
+REPEAT
+SELECT quantityInStock INTO quantity_in_stock
+FROM products
+WHERE productCode = prd_code;
+
+IF quantity_in_stock < 100 THEN
+INSERT INTO infologs(msg)
+VALUES (prd_code);
+END IF;
+FETCH cur_product INTO prd_code;
+UNTIL no_more_products = 1
+END REPEAT;
+CLOSE cur_product;
+SELECT * FROM infologs;
+DROP TABLE infologs;
+END$$
+DELIMITER;
+############################################################################################
+
+** Make sure DECLARE statement is immediate next to BEGIN statement of procedure.
+** Error Code : 1338 - Cursor declaration after handler declaration - Declaration of cursor should be handler
+
+
+
+
+
+TRIGGER
+========================================================================================================================
+DELIMITER $$
+CREATE TRIGGER `edi_TR_INSERT` AFTER INSERT ON `edi`
+
+    FOR EACH ROW BEGIN
+        DECLARE count_a INT;
+	UPDATE fcl_file_number SET edi_status = NEW.status, edi_success = NEW.success WHERE id = NEW.fcl_file_number_id;
+	SELECT id FROM FILE INTO count_a;
+    END;
+$$
+
+** Error Code: 1422. Explicit or implicit commit is not allowed in stored function or trigger
+ANS : Creation or update of tables or other such actions are not allowed in function or trigger.
+      Basically we can insert or update the content of the tables only.
+
+
+FUNCTION
+========================================================================================================================
+DELIMITER $$
+DROP FUNCTION IF EXISTS `user_FN`$$
+
+CREATE FUNCTION `user_FN`(param_id INT) RETURNS TEXT
+    READS SQL DATA
+    DETERMINISTIC
+    BEGIN
+	DECLARE full_name TEXT;
+	IF (param_id != '') THEN
+		SELECT CONCAT(u.first_name,' ',edi_TR_UPDATE(u.last_name,'')) INTO full_name FROM USER u WHERE u.id = param_id;
+		RETURN full_name;
+	END IF;
+	RETURN '';
+    END$$
+
+DELIMITER ;
+
+** it is RETURNS not RETURN
---
+++ out/production/core/all/material/javascript.js
@@ -0,0 +1,331 @@
+//Which keywords are used to handle exceptions?
+//------------------------------------------------------------------------------------------------------------------------
+
+try {
+    //Code
+} catch (exp) {
+    //Code to throw an exception
+} finally {
+    //Code runs either it finishes successfully or after catch
+}
+
+/*
+ What is variable typing?
+ ------------------------------------------------------------------------------------------------------------------------
+ Variable typing is used to assign a number to a variable and then assign string to the same variable. Example is as follows:
+ */
+
+i = 8;
+i = "john";
+
+
+
+
+//Client Operating system
+//------------------------------------------------------------------------------------------------------------------------*/
+navigator.platform
+
+
+//How to find complete system information in the client machine using JavaScript?
+//------------------------------------------------------------------------------------------------------------------------
+navigator.appVersion
+
+"5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+
+
+//push, unshift() and pop(), shift()
+//------------------------------------------------------------------------------------------------------------------------
+//All there are arrays functions
+
+push()     //-> it add the element at the bottom of array
+unshift()  //-> add the item at the start of array
+
+pop()      //-> remove the item from the end of array
+shift()   //-> remove the item the starting of array
+
+
+
+
+//What is the 'Strict' mode in JavaScript and how can it be enabled?
+//------------------------------------------------------------------------------------------------------------------------
+//The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+
+"use strict";
+x = 3.14;       // This will cause an error because x is not declared
+
+
+//Explain window.onload and onDocumentReady?
+//------------------------------------------------------------------------------------------------------------------------
+//The onload function is not run until all the information on the page is loaded. This leads to a substantial delay before any code is executed.
+
+//onDocumentReady loads the code just after the DOM is loaded. This allows early manipulation of the code.
+
+
+//Creating Objects
+//------------------------------------------------------------------------------------------------------------------------
+
+//1. Factory Function 
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log("draw with radius : " + this.radius);
+        }
+    }
+}
+
+let circle = createCircle(5);   //<-- not there is no new operator here.
+circle.draw();
+
+
+//2. Constructor Function
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log("Circle draw : " + this.radius);
+    }
+}
+
+var circle = new Circle(5);
+circle.draw();
+
+
+//Create private variable and functions
+//------------------------------------------------------------------------------------------------------------------------
+
+/*There is no direct way to make it private however local variable or function can act as private variable and function.
+ This is possible because of closures.*/
+
+function Circle(radius) {
+
+    let colors = ['red', 'green'];  //local or private function
+
+    function printWithColor(size) {  //local or private function
+        console.log("size is " + size + " with colors " + colors);
+    }
+
+    this.radius = radius;
+
+    this.draw = function () {
+        printWithColor(this.radius); //<-- not this keyword is used since it is instance variable.
+    };
+}
+
+var circle = new Circle(5);
+circle.draw(5);
+
+
+//Setters and Getters
+//------------------------------------------------------------------------------------------------------------------------
+
+// Method 1
+
+function Circle(radius) {
+
+    let colors = ['red', 'green'];
+
+    this.getAvailableColors = function () {   //<--- Getters which is class level function accessing the local variable
+        return colors;
+    };
+
+    this.radius = radius;
+
+    this.draw = function () {
+        console.log("draw " + this.radius);
+    };
+
+}
+
+var circle = new Circle(5);
+circle.draw(5);
+
+
+// Method 2
+
+function Circle(radius) {
+
+    let colors = ['red', 'green'];
+    //Getters
+    this.getAvailableColors = function () {
+        return colors;
+    };
+
+    Object.defineProperty(this, "colors", {
+        get: function () {
+            console.log("getting colors " + colors);
+            return colors;
+        },
+        set: function (value) {
+            console.log("setting colors " + value);
+            colors = value;
+        }
+    });
+    
+    Object.defineProperties(this, {
+        'property1': {
+          value: true,
+          writable: true
+        },
+        'property2': {
+          value: 'Hello',
+          writable: false
+        }
+      });
+      
+}
+
+var circle = new Circle(5);
+circle.colors;              // Getter method is called
+circle.colors = ["yellow"]; // Setter method is called
+
+
+//delete property
+
+delete circle.radius;
+
+//call vs apply vs bind
+//------------------------------------------------------------------------------------------------------------------------
+//The function .call() and .apply() are very similar in their usage except a little difference. .call() is used when 
+//the number of the function's arguments are known to the programmer, as they have to be mentioned as arguments in the 
+//call statement. On the other hand, .apply() is used when the number is not known. The function .apply() expects the 
+//argument to be an array.
+
+Circle.call({}, 4);     //<-- first argument decides the context this, second arguments are arguments of called function Circle.
+
+Circle.apply({}, [4]);  // functions are arguments are passed as an array. that is the only difference from the call.
+
+var newCircle = Circle.bind({}); //<-- Bind will create new function attached to the current object which can be called later.
+newCircle(4);
+
+
+
+//Object important functions
+//------------------------------------------------------------------------------------------------------------------------
+
+//Loop over the each key of object
+
+for (let key in circle) {
+    if (typeof circle[key] !== 'function') {
+        console.log(key, circle[key]);
+    }
+}
+
+//returns array of keys
+
+Object.keys(circle);
+
+
+//returns the array of values
+
+Object.values(circle);
+
+
+//check if the property exist in the object
+
+if ('radius' in circle) {
+    console.log("Circle has the radius property or function");
+}
+
+
+//Closures
+//----------------------------------------------------------------------------------------------------------------------
+//Closures determines which variables will be accessible in inner functions.
+
+
+/**
+ * Array Operators
+ */
+//----------------------------------------------------------------------------------------------------------------------
+
+let items = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+//For each - array.forEach(function(currentValue, index, arr), thisValue)
+items.forEach((item, index) => {
+    console.log(item, index);
+});
+
+//Map - array.map(function(currentValue, index, arr), thisValue)
+let modifiedDays = items.map(item => {
+    return {name: "sunil", day: item};
+});
+
+//Filter - array.filter(function(currentValue, index, arr), thisValue)
+let holidays = items.filter(item => {
+    return item.startsWith("S");
+});
+
+//reduce - array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+let concateDays = items.reduce((acc, item) => {
+    acc += " " + item;
+    return acc;
+}, "Days ");  //<-- initial value
+
+
+/**
+ * 
+ * Inheritance
+ * --------------------------------------------------------------------------------------------------------------------
+ */
+
+//Method 1
+function Vehicle(name) {
+
+    this.name = name;
+
+    this.print = function () {
+        console.log(this.name);
+    }
+}
+
+function Car(name) {
+    Vehicle.call(this, name);         //<-- all methods and propeties will be copied to the Car.
+}
+
+let vehicle = new Vehicle("BMW");
+vehicle.print();
+
+let car = new Car("Maruti");
+car.print();
+
+
+//Method 2
+function Vehicle(name) {
+    this.name = name;
+    this.print = function () {
+        console.log(this.name);
+    };
+}
+
+function Car() {
+}
+
+Car.prototype = new Vehicle("Ferrari");  //<-- the properties and methods will go under __proto__ not at the class.
+let car = new Car();
+
+car.print();
+
+
+/**
+ * Creating Singleton 
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+//Method 1
+var bike = new function(name){
+	this.name = name;
+	this.print = function(){
+		console.log("This is bike ", this.name);
+	};
+}("KTM");
+
+
+//Method 2
+var bike = {
+    name : "KTM",
+    print : function(){
+        console.log("This is bike ", this.name);
+    }
+    
+};
